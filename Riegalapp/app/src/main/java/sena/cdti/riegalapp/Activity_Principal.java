3447742568f9c7334d4b5a4fa2c52ff241308881
Sebastián Cifuentes <jscifuentes31@misena.edu.co>
2017-02-27 15:25:10 -0500

    Importación de imágenes RiegalApp

--- Riegalapp/app/src/main/java/sena/cdti/riegalapp/Activity_Principal.java
+++ Riegalapp/app/src/main/java/sena/cdti/riegalapp/Activity_Principal.java
@@ -23,11 +23,11 @@ public class Activity_Principal extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Bien Venido", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
